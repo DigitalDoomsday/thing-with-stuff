ce9712999e3cdbfda0e976f1c9888dea7b88a5c7
DigitalDoomsday <v1rtu4l.hello@gmail.com>
2025-07-28 22:11:26 -0500

    Update trolltraptower.lua

--- trolltraptower.lua
+++ trolltraptower.lua
@@ -1,3 +1,12 @@
+--[[
+	Roblox2Lua
+	----------
+	
+	This code was generated using
+	Deluct's Roblox2Lua plugin.
+]]--
+
+--// Instances
 function getglove()
 	for i, v in pairs(game.Players:GetPlayers()) do
 		if v.Character then
@@ -21,7 +30,7 @@ function getglove()
 		end
 	end
 end 
-drag = nil
+local drag = nil
 if drag == nil then
 	drag = function(f)
 		if f then
@@ -33,21 +42,23 @@ end
 local players = game:GetService("Players")
 local localplr = players.LocalPlayer
 local thegui = Instance.new("ScreenGui")
-thegui.IgnoreGuiInset = false
-thegui.ResetOnSpawn = false
-thegui.ZIndexBehavior = Enum.ZIndexBehavior.Global
-thegui.Name = "12952456875486445983783452227635151277232954895541"
-thegui.Parent = game.CoreGui
+local _12952456875486445983783452227635151277232954895541 = Instance.new("ScreenGui")
+_12952456875486445983783452227635151277232954895541.IgnoreGuiInset = false
+_12952456875486445983783452227635151277232954895541.ResetOnSpawn = false
+_12952456875486445983783452227635151277232954895541.ZIndexBehavior = Enum.ZIndexBehavior.Global
+_12952456875486445983783452227635151277232954895541.Name = "12952456875486445983783452227635151277232954895541"
+_12952456875486445983783452227635151277232954895541.Parent = workspace
 
 local frame = Instance.new("Frame")
+frame.Active = true
 frame.BackgroundColor3 = Color3.new(0.172549, 0.172549, 0.172549)
 frame.BorderColor3 = Color3.new(0, 0, 0)
 frame.BorderSizePixel = 0
 frame.Position = UDim2.new(0.330529869, 0, 0.31898731, 0)
 frame.Size = UDim2.new(0, 402, 0, 228)
 frame.Visible = true
-frame.Parent = thegui
-drag(frame)
+frame.Parent = _12952456875486445983783452227635151277232954895541
+
 local title = Instance.new("TextLabel")
 title.Font = Enum.Font.FredokaOne
 title.Text = "top 5 reasons why i hate noobs"
@@ -56,7 +67,7 @@ title.TextSize = 14
 title.BackgroundColor3 = Color3.new(0, 0.784314, 1)
 title.BorderColor3 = Color3.new(0, 0, 0)
 title.BorderSizePixel = 0
-title.Position = UDim2.new(0, 0, - 0.0760534182, 0)
+title.Position = UDim2.new(0, 0, -0.0760534182, 0)
 title.Size = UDim2.new(0, 402, 0, 24)
 title.Visible = true
 title.ZIndex = 3
@@ -75,41 +86,13 @@ sortinventory.TextSize = 14
 sortinventory.BackgroundColor3 = Color3.new(0.25098, 0.25098, 0.25098)
 sortinventory.BorderColor3 = Color3.new(0, 0, 0)
 sortinventory.BorderSizePixel = 0
-sortinventory.Position = UDim2.new(0.393034816, 0, 0.0582792722, 0)
+sortinventory.Position = UDim2.new(0.25, 0, 0.0582792722, 0)
 sortinventory.Size = UDim2.new(0, 85, 0, 21)
 sortinventory.Visible = true
 sortinventory.ZIndex = 2
 sortinventory.Name = "sort inventory"
 sortinventory.Parent = frame
-sortinventory.MouseButton1Click:Connect(function()
-	local bckpck = game.Players.LocalPlayer.Backpack
-	if bckpck.GoldHand and bckpck.MagicCarpet and bckpck.LaserGun then
-		for i, v in bckpck:GetChildren() do
-			if v == bckpck.GoldHand or v == bckpck.MagicCarpet or v == bckpck.LaserGun then
-				print("goodie")
-			else
-				v:Destroy()
-			end
-		end
-		wait(0.1)
-		bckpck.GoldHand.Parent = game.Players.LocalPlayer
-		bckpck.MagicCarpet.Parent = game.Players.LocalPlayer
-		bckpck.LaserGun.Parent = game.Players.LocalPlayer
-		wait()
-		game.Players.LocalPlayer.GoldHand.Parent = bckpck
-		wait()
-		game.Players.LocalPlayer.MagicCarpet.Parent = bckpck
-		wait()
-		game.Players.LocalPlayer.LaserGun.Parent = bckpck
-		wait()
-	else
-		mainfsortinventoryrame2.Text = "you dont have the stuff"
-		wait(1)
-		sortinventory.Text = "nigger"
-		wait(0.1)
-		sortinventory.Text = "sort inventory"
-	end
-end)
+
 local uicorner_2 = Instance.new("UICorner")
 uicorner_2.CornerRadius = UDim.new(0, 10)
 uicorner_2.Parent = sortinventory
@@ -126,7 +109,6 @@ drop_shadow_holder.Parent = sortinventory
 local drop_shadow = Instance.new("ImageLabel")
 drop_shadow.Image = "rbxassetid://6014261993"
 drop_shadow.ImageColor3 = Color3.new(0, 0, 0)
-
 drop_shadow.ImageTransparency = 0.6399999856948853
 drop_shadow.ScaleType = Enum.ScaleType.Slice
 drop_shadow.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -153,78 +135,7 @@ antislap.Visible = true
 antislap.ZIndex = 2
 antislap.Name = "Anti slap"
 antislap.Parent = frame
-local green = Color3.new(0, 0.976806, 0)
-local red = Color3.new(0, 0.976806, 0)
-local twininfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0)
-local twinservice = game:GetService("TweenService")
-local twingreen = twinservice:Create(antislap, twininfo, {
-	BackgroundColor3 = green
-})
-local twinred = twinservice:Create(antislap, twininfo, {
-	BackgroundColor3 = red
-})
-local asf = false
-antislap.MouseButton1Click:Connect(function()
-	asf = not asf
-	wait()
-	if asf then
-		asf = false
-		if localplr.Character then
-			if not localplr:FindFirstChild("thedetector!!") then
-				local f = Instance.new("AtmosphereSensor", localplr)
-				f.Name = "thedetector!!"
-				for i, v in pairs(localplr.Character:GetChildren()) do
-					dobv(v, localplr.Character)
-					for i, v in pairs(v:GetChildren()) do
-						dobv(v, localplr.Character)
-					end
-				end
-			else 
 
-			end
-		end
-		wait()
-		twingreen:Play()
-	else
-		wait()
-		twinred:Play()
-	end
-end)
-function dobv(v, char)
-	local undo = false
-	if as then
-		if v:IsA("BodyAngularVelocity") then
-			undo = true
-			v:Destroy()
-		elseif v:IsA("BodyGyro") and v.MaxTorque ~= Vector3.new(8999999488, 8999999488, 8999999488) and v.D ~= 500 and v.D ~= 50 and v.P ~= 90000 then
-			undo = true
-			v:Destroy()
-		elseif v:IsA("BodyVelocity") and v.MaxForce ~= Vector3.new(8999999488, 8999999488, 8999999488) and v.Velocity ~= Vector3.new(0, 0, 0) then
-			undo = true
-			v:Destroy()
-		elseif v:IsA("BasePart") then
-			v.ChildAdded:Connect(function(v2)
-				dobv(v2, char)
-			end)
-		end
-		if undo and char and char:FindFirstChild("Humanoid") then
-			char.Humanoid.Sit = false
-			char.Humanoid.PlatformStand = false
-		end
-	end
-end
-
-function dc(c)
-	for i, v in pairs(c:GetChildren()) do
-		dobv(v, c)
-	end
-	c.ChildAdded:Connect(function(v)
-		dobv(v, c)
-	end)
-end
-if localplr.Character then
-	dc(localplr.Character)
-end
 local uicorner_3 = Instance.new("UICorner")
 uicorner_3.CornerRadius = UDim.new(0, 10)
 uicorner_3.Parent = antislap
@@ -243,16 +154,7 @@ killall.Visible = true
 killall.ZIndex = 2
 killall.Name = "kill all"
 killall.Parent = frame
-killall.MouseButton1Click:Connect(function()
-	local glove, event = getglove()
-	if event then
-		for i, v in pairs(game.Players:GetPlayers()) do
-			if v ~= localplr and v.Character and v.UserId ~= 1630166532 and v.UserId ~= 7278805869 and v.UserId ~= 603492961 then
-				event:FireServer("slash", v.Character, Vector3.new(0, - tonumber("inf"), 0))
-			end
-		end
-	end
-end)
+
 local uicorner_4 = Instance.new("UICorner")
 uicorner_4.CornerRadius = UDim.new(0, 10)
 uicorner_4.Parent = killall
@@ -271,16 +173,7 @@ flingall.Visible = true
 flingall.ZIndex = 2
 flingall.Name = "fling all"
 flingall.Parent = frame
-flingall.MouseButton1Click:Connect(function()
-	local glove, event = getglove()
-	if event then
-		for i, v in pairs(game.Players:GetPlayers()) do
-			if v ~= localplr and v.Character and v.UserId ~= 1630166532 and v.UserId ~= 7278805869 and v.UserId ~= 603492961 then
-				event:FireServer("slash", v.Character, Vector3.new(0, tonumber("inf"), 0))
-			end
-		end
-	end
-end)
+
 local uicorner_5 = Instance.new("UICorner")
 uicorner_5.CornerRadius = UDim.new(0, 10)
 uicorner_5.Parent = flingall
@@ -314,6 +207,129 @@ bounceallovertheplace.MouseButton1Click:Connect(function()
 		end
 	end
 end)
+flingall.MouseButton1Click:Connect(function()
+	local glove, event = getglove()
+	if event then
+		for i, v in pairs(game.Players:GetPlayers()) do
+			if v ~= localplr and v.Character and v.UserId ~= 1630166532 and v.UserId ~= 7278805869 and v.UserId ~= 603492961 then
+				event:FireServer("slash", v.Character, Vector3.new(0, tonumber("inf"), 0))
+			end
+		end
+	end
+end)
+killall.MouseButton1Click:Connect(function()
+	local glove, event = getglove()
+	if event then
+		for i, v in pairs(game.Players:GetPlayers()) do
+			if v ~= localplr and v.Character and v.UserId ~= 1630166532 and v.UserId ~= 7278805869 and v.UserId ~= 603492961 then
+				event:FireServer("slash", v.Character, Vector3.new(0, - tonumber("inf"), 0))
+			end
+		end
+	end
+end)
+local as = false
+function dobv(v, char)
+	local undo = false
+	if as then
+		if v:IsA("BodyAngularVelocity") then
+			undo = true
+			v:Destroy()
+		elseif v:IsA("BodyGyro") and v.MaxTorque ~= Vector3.new(8999999488, 8999999488, 8999999488) and v.D ~= 500 and v.D ~= 50 and v.P ~= 90000 then
+			undo = true
+			v:Destroy()
+		elseif v:IsA("BodyVelocity") and v.MaxForce ~= Vector3.new(8999999488, 8999999488, 8999999488) and v.Velocity ~= Vector3.new(0, 0, 0) then
+			undo = true
+			v:Destroy()
+		elseif v:IsA("BasePart") then
+			v.ChildAdded:Connect(function(v2)
+				dobv(v2, char)
+			end)
+		end
+		if undo and char and char:FindFirstChild("Humanoid") then
+			char.Humanoid.Sit = false
+			char.Humanoid.PlatformStand = false
+		end
+	end
+end
+
+function dc(c)
+	for i, v in pairs(c:GetChildren()) do
+		dobv(v, c)
+	end
+	c.ChildAdded:Connect(function(v)
+		dobv(v, c)
+	end)
+end
+local green = Color3.new(0, 0.976806, 0)
+local red = Color3.new(0, 0.976806, 0)
+local twininfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0)
+local twinservice = game:GetService("TweenService")
+local twingreen = twinservice:Create(antislap, twininfo, {
+	BackgroundColor3 = green
+})
+local twinred = twinservice:Create(antislap, twininfo, {
+	BackgroundColor3 = red
+})
+local asf = false
+sortinventory.MouseButton1Click:Connect(function()
+	local bckpck = game.Players.LocalPlayer.Backpack
+	if bckpck.GoldHand and bckpck.MagicCarpet and bckpck.LaserGun then
+		for i, v in bckpck:GetChildren() do
+			if v == bckpck.GoldHand or v == bckpck.MagicCarpet or v == bckpck.LaserGun then
+				print("goodie")
+			else
+				v:Destroy()
+			end
+		end
+		wait(0.1)
+		bckpck.GoldHand.Parent = game.Players.LocalPlayer
+		bckpck.MagicCarpet.Parent = game.Players.LocalPlayer
+		bckpck.LaserGun.Parent = game.Players.LocalPlayer
+		wait()
+		game.Players.LocalPlayer.GoldHand.Parent = bckpck
+		wait()
+		game.Players.LocalPlayer.MagicCarpet.Parent = bckpck
+		wait()
+		game.Players.LocalPlayer.LaserGun.Parent = bckpck
+		wait()
+	else
+		sortinventory.Text = "you dont have the stuff"
+		wait(1)
+		sortinventory.Text = "nigger"
+		wait(0.1)
+		sortinventory.Text = "sort inventory"
+	end
+end)
+antislap.MouseButton1Click:Connect(function()
+	asf = not asf
+	wait()
+	if asf then
+		asf = false
+		if localplr.Character then
+			if not localplr:FindFirstChild("thedetector!!") then
+				local f = Instance.new("AtmosphereSensor", localplr)
+				f.Name = "thedetector!!"
+				for i, v in pairs(localplr.Character:GetChildren()) do
+					dobv(v, localplr.Character)
+					for i, v in pairs(v:GetChildren()) do
+						dobv(v, localplr.Character)
+					end
+				end
+			else 
+
+			end
+		end
+		wait()
+		twingreen:Play()
+	else
+		wait()
+		twinred:Play()
+	end
+end)
+
+if localplr.Character then
+	dc(localplr.Character)
+end
 local uicorner_6 = Instance.new("UICorner")
 uicorner_6.CornerRadius = UDim.new(0, 10)
 uicorner_6.Parent = bounceallovertheplace
@@ -328,7 +344,7 @@ shadows.Parent = frame
 local mainshadow = Instance.new("Frame")
 mainshadow.BackgroundTransparency = 1
 mainshadow.BorderSizePixel = 0
-mainshadow.Position = UDim2.new(0, -5, - 0.0900000036, 0)
+mainshadow.Position = UDim2.new(0, -5, -0.0900000036, 0)
 mainshadow.Size = UDim2.new(1.01990044, 0, 1.10236895, 0)
 mainshadow.Visible = true
 mainshadow.ZIndex = 0
@@ -338,6 +354,7 @@ mainshadow.Parent = shadows
 local drop_shadow_2 = Instance.new("ImageLabel")
 drop_shadow_2.Image = "rbxassetid://6014261993"
 drop_shadow_2.ImageColor3 = Color3.new(0, 0, 0)
+
 drop_shadow_2.ImageTransparency = 0.30000001192092896
 drop_shadow_2.ScaleType = Enum.ScaleType.Slice
 drop_shadow_2.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -364,6 +381,7 @@ drop_shadodw_holder.Parent = shadows
 local drop_shadow_3 = Instance.new("ImageLabel")
 drop_shadow_3.Image = "rbxassetid://6014261993"
 drop_shadow_3.ImageColor3 = Color3.new(0, 0, 0)
+
 drop_shadow_3.ImageTransparency = 0.6000000238418579
 drop_shadow_3.ScaleType = Enum.ScaleType.Slice
 drop_shadow_3.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -389,6 +407,7 @@ drop_shadodw_holder_2.Parent = shadows
 local drop_shadow_4 = Instance.new("ImageLabel")
 drop_shadow_4.Image = "rbxassetid://6014261993"
 drop_shadow_4.ImageColor3 = Color3.new(0, 0, 0)
+
 drop_shadow_4.ImageTransparency = 0.6000000238418579
 drop_shadow_4.ScaleType = Enum.ScaleType.Slice
 drop_shadow_4.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -414,6 +433,7 @@ drop_shadodw_holder_3.Parent = shadows
 local drop_shadow_5 = Instance.new("ImageLabel")
 drop_shadow_5.Image = "rbxassetid://6014261993"
 drop_shadow_5.ImageColor3 = Color3.new(0, 0, 0)
+
 drop_shadow_5.ImageTransparency = 0.6000000238418579
 drop_shadow_5.ScaleType = Enum.ScaleType.Slice
 drop_shadow_5.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -439,6 +459,7 @@ drop_shadodw_holder_4.Parent = shadows
 local drop_shadow_6 = Instance.new("ImageLabel")
 drop_shadow_6.Image = "rbxassetid://6014261993"
 drop_shadow_6.ImageColor3 = Color3.new(0, 0, 0)
+
 drop_shadow_6.ImageTransparency = 0.6000000238418579
 drop_shadow_6.ScaleType = Enum.ScaleType.Slice
 drop_shadow_6.SliceCenter = Rect.new(49, 49, 450, 450)
@@ -450,3 +471,50 @@ drop_shadow_6.Size = UDim2.new(1, 47, 1, 47)
 drop_shadow_6.Visible = true
 drop_shadow_6.Name = "DropShadow"
 drop_shadow_6.Parent = drop_shadodw_holder_4
+
+local tptobutton = Instance.new("TextButton")
+tptobutton.Font = Enum.Font.FredokaOne
+tptobutton.Text = "tp to button"
+tptobutton.TextColor3 = Color3.new(1, 1, 1)
+tptobutton.TextSize = 14
+tptobutton.BackgroundColor3 = Color3.new(0.25098, 0.25098, 0.25098)
+tptobutton.BorderColor3 = Color3.new(0, 0, 0)
+tptobutton.BorderSizePixel = 0
+tptobutton.Position = UDim2.new(0.5, 0, 0.0582792722, 0)
+tptobutton.Size = UDim2.new(0, 85, 0, 21)
+tptobutton.Visible = true
+tptobutton.ZIndex = 2
+tptobutton.Name = "tp to button"
+tptobutton.Parent = frame
+tptobutton.MouseButton1Click:Connect(function()
+	local hrp = localplr.Character:FindFirstChild("HumanoidRootPart")
+	hrp.CFrame = CFrame.new(766.5, 198.499954, 55.4999924, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+end)
+local uicorner_8 = Instance.new("UICorner")
+uicorner_8.CornerRadius = UDim.new(0, 10)
+uicorner_8.Parent = tptobutton
+
+local drop_shadow_holder_2 = Instance.new("Frame")
+drop_shadow_holder_2.BackgroundTransparency = 1
+drop_shadow_holder_2.BorderSizePixel = 0
+drop_shadow_holder_2.Size = UDim2.new(1, 0, 1, 0)
+drop_shadow_holder_2.Visible = true
+drop_shadow_holder_2.ZIndex = 2
+drop_shadow_holder_2.Name = "DropShadowHolder"
+drop_shadow_holder_2.Parent = tptobutton
+
+local drop_shadow_7 = Instance.new("ImageLabel")
+drop_shadow_7.Image = "rbxassetid://6014261993"
+drop_shadow_7.ImageColor3 = Color3.new(0, 0, 0)
+
+drop_shadow_7.ImageTransparency = 0.6399999856948853
+drop_shadow_7.ScaleType = Enum.ScaleType.Slice
+drop_shadow_7.SliceCenter = Rect.new(49, 49, 450, 450)
+drop_shadow_7.AnchorPoint = Vector2.new(0.5, 0.5)
+drop_shadow_7.BackgroundTransparency = 1
+drop_shadow_7.BorderSizePixel = 0
+drop_shadow_7.Position = UDim2.new(0.5, 0, 0.5, 0)
+drop_shadow_7.Size = UDim2.new(1, 25, 1, 30)
+drop_shadow_7.Visible = true
+drop_shadow_7.Name = "DropShadow"
+drop_shadow_7.Parent = drop_shadow_holder_2
